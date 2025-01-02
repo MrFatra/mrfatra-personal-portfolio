deb0dbeb68bc3552110f955487d2b8d098c230f5
MrFatra <ramadhanfatra2004@gmail.com>
2025-01-02 21:20:43 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { VStack } from '@chakra-ui/react'
+import { Header } from './sections'
+
+function App() {
+
+  return (
+    <VStack>
+      <Header />
+    </VStack>
+  )
+}
+
+export default App
