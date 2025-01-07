4d295259cf3eaabfd5f6414f1ddcfaa97a637065
MrFatra <ramadhanfatra2004@gmail.com>
2025-01-07 08:56:01 +0700

    feat: enhance footer with adding spotify song

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,27 @@
 import { useColorMode } from '@/components/ui/color-mode'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Image, Link, Text } from '@chakra-ui/react'
 
 const Footer = () => {
     const { colorMode } = useColorMode()
 
     return (
-        <Box id='footer' textAlign={'center'} mb={20}>
-            <Text fontSize={'sm'} color={colorMode === 'dark' ? 'gray.400' : 'black'}>Copyright © {new Date().getFullYear()} By MrFatra</Text>
+        <Box id='footer' textAlign={'center'} mb={10} display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'} gap={10}>
             <Box fontSize={'sm'} fontWeight={'medium'} color={colorMode === 'dark' ? 'gray.600' : 'gray.400'}>
+                <Text fontSize={'sm'} color={colorMode === 'dark' ? 'gray.400' : 'black'}>Copyright © {new Date().getFullYear()} By MrFatra</Text>
                 <Text>"Tell me if it's ok."</Text>
                 <Text fontSize="sm">
                     "Because I'm not a great programmer; I'm just a good programmer with great habits."
                 </Text>
+            </Box>
+            <Box w={200}>
+                <Link href="https://spotify-github-profile.kittinanx.com/api/view?uid=w9ucj7q3vi5hjgbebqmmfj0rb&redirect=true" target="_blank">
+                    <Image
+                        src="https://spotify-github-profile.kittinanx.com/api/view?uid=w9ucj7q3vi5hjgbebqmmfj0rb&cover_image=true&theme=novatorem&show_offline=false&background_color=b33737&interchange=true&bar_color=53b14f&bar_color_cover=true"
+                        alt="Spotify GitHub Profile"
+                        bg={colorMode === 'dark' ? 'whiteAlpha.300' : 'blackAlpha.300'}
+                        borderRadius={'lg'}
+                    />
+                </Link>
             </Box>
         </Box>
     )
